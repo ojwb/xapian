d2052d3cd31433bcf281bbd0329093a7d9d168dc
Olly Betts <olly@survex.com>
2016-05-23 17:04:21 +1200

    Add missing new header: xapian/rset.h

---
+++ xapian-core/include/xapian/rset.h
@@ -0,0 +1,25 @@
+#ifndef XAPIAN_INCLUDED_RSET_H
+#define XAPIAN_INCLUDED_RSET_H
+
+#include <xapian/intrusive_ptr.h>
+#include <xapian/types.h>
+#include <xapian/visibility.h>
+
+namespace Xapian {
+
+class XAPIAN_VISIBILITY_DEFAULT RSet {
+  public:
+    class Internal;
+    Xapian::Internal::intrusive_ptr<Internal> internal;
+    RSet();
+    RSet(const RSet& o);
+    ~RSet();
+    RSet& operator=(const RSet& o);
+    void add_document(Xapian::docid did);
+    Xapian::doccount size() const;
+    bool empty() const { return size() == 0; }
+};
+
+}
+
+#endif
