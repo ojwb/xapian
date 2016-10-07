9601375e6d74c3cf4f9daa30d051274d6ccd2e3d
Olly Betts <olly@survex.com>
2016-10-08 10:18:35 +1300

    Revert "FIXME - nasty patch to allow testing wildcards"
    This reverts commit f6e2dbc9074906de54ead30e742c93ca7b6501db.

--- xapian-core/examples/quest.cc
+++ xapian-core/examples/quest.cc
@@ -359,7 +359,10 @@ try {
     parser.set_stemming_strategy(Xapian::QueryParser::STEM_SOME);
     parser.set_stopper(&mystopper);
 
-    Xapian::Query query(Xapian::Query::OP_WILDCARD, argv[optind], 0, Xapian::Query::WILDCARD_PATTERN_GLOB);
+    Xapian::Query query = parser.parse_query(argv[optind], flags);
+    const string & correction = parser.get_corrected_query_string();
+    if (!correction.empty())
+	cout << "Did you mean: " << correction << "\n\n";
 
     cout << "Parsed Query: " << query.get_description() << endl;
 
