854d70cad9c660df03b83ea777a7ac6128a5d1c4
Olly Betts <olly@survex.com>
2023-07-21 13:21:58 +1200

    Debug kill_remote() on mingw

--- xapian-core/tests/harness/backendmanager_remotetcp.cc
+++ xapian-core/tests/harness/backendmanager_remotetcp.cc
@@ -47,6 +47,7 @@
 #include "errno_to_string.h"
 #include "str.h"
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -117,6 +118,10 @@ class ServerData {
     }
 
     bool kill_remote(const void* dbi) {
+#ifdef __WIN32__
+	cout << "ServerData::kill_remote(" << (unsigned long)(dbi) << ")\n";
+	cout << "  pid = " << pid << ", db_internal = " << (unsigned long)db_internal << '\n';
+#endif
 	if (pid == UNUSED_PID || dbi != db_internal) return false;
 #ifdef HAVE_FORK
 	// Kill the process group that we put the server in so that we kill
@@ -130,6 +135,9 @@ class ServerData {
 	    throw Xapian::DatabaseError("Couldn't kill remote server",
 					-GetLastError());
 	}
+#ifdef __WIN32__
+	cout << "  killed!\n";
+#endif
 #endif
 	clean_up();
 	pid = UNUSED_PID;
@@ -445,9 +453,16 @@ void
 BackendManagerRemoteTcp::kill_remote(const Xapian::Database& db)
 {
     const void* db_internal = db.internal.get();
+#ifdef __WIN32__
+    cout << "BackendManagerRemoteTcp::kill_remote(" << (unsigned long)(db_internal) << ")\n";
+#endif
     for (unsigned i = 0; i != first_unused_server_data; ++i) {
-	if (server_data[i].kill_remote(db_internal))
+	if (server_data[i].kill_remote(db_internal)) {
+#ifdef __WIN32__
+	    cout << "  -> shard #" << i << '\n';
+#endif
 	    return;
+	}
     }
     throw Xapian::DatabaseError("No known server for remote DB");
 }
