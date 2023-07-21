c87b9940a89489282ca5fda535833f36852f3eda
Olly Betts <olly@survex.com>
2023-07-22 07:23:13 +1200

    ucrt debug

--- xapian-core/ucrttest.cpp
+++ xapian-core/ucrttest.cpp
@@ -1,7 +1,7 @@
 #define PACKAGE "xapian-core"
 #define VERSION "1.5.0"
 #define _FILE_OFFSET_BITS 64
-#define __MSVCRT_VERSION__ 0x0601
+//#define __MSVCRT_VERSION__ 0x0601
 #define MINGW_HAS_SECURE_API 1
 #include <stddef.h>
 #include <stdio.h>
