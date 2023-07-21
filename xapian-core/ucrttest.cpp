b53e961eae25ed84e07ff3fe68a566d8bd381dc2
Olly Betts <olly@survex.com>
2023-07-21 18:04:48 +1200

    try to reduce ucrttest.cpp

--- xapian-core/ucrttest.cpp
+++ xapian-core/ucrttest.cpp
@@ -3,21 +3,7 @@
 #define _FILE_OFFSET_BITS 64
 #define __MSVCRT_VERSION__ 0x0601
 #define MINGW_HAS_SECURE_API 1
-#define ssize_t int
-#define pid_t __int64
-#define mode_t int
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <inttypes.h>
-#include <stdint.h>
-#include <strings.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-static long int longval (void) { return (long int) (sizeof (short)); }
-static unsigned long int ulongval (void) { return (long int) (sizeof (short)); }
 #include <stdio.h>
 #include <stdlib.h>
 int main (void) {}
