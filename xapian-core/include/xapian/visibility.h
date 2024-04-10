c592dae4f805a589b6372f91d7907335999f1985
Olly Betts <olly@survex.com>
2024-04-11 09:15:15 +1200

    Support building DLL with MSVC

--- xapian-core/include/xapian/visibility.h
+++ xapian-core/include/xapian/visibility.h
@@ -1,7 +1,7 @@
 /** @file
  * @brief Define XAPIAN_VISIBILITY_* macros.
  */
-// Copyright (C) 2007,2010,2014,2017,2019 Olly Betts
+// Copyright (C) 2007,2010,2014,2017,2019,2024 Olly Betts
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -27,6 +27,14 @@
 #ifdef XAPIAN_ENABLE_VISIBILITY
 # define XAPIAN_VISIBILITY_DEFAULT __attribute__((visibility("default")))
 # define XAPIAN_VISIBILITY_INTERNAL __attribute__((visibility("internal")))
+#elif defined(_MSC_VER)
+# ifdef XAPIAN_LIB_BUILD
+#  define XAPIAN_VISIBILITY_DEFAULT __declspec(dllexport)
+#  define XAPIAN_VISIBILITY_INTERNAL
+# else
+#  define XAPIAN_VISIBILITY_DEFAULT __declspec(dllimport)
+#  define XAPIAN_VISIBILITY_INTERNAL
+# endif
 #else
 # define XAPIAN_VISIBILITY_DEFAULT
 # define XAPIAN_VISIBILITY_INTERNAL
