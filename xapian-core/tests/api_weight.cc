52e7ff3806368fc0e94862e88a013def16bc2bff
Olly Betts <olly@survex.com>
2024-06-20 16:41:17 +1200

    Check fix with nonempty varargs

--- xapian-core/tests/api_weight.cc
+++ xapian-core/tests/api_weight.cc
@@ -3,6 +3,9 @@ namespace Xapian {
     struct BoolWeight : public Weight {
 	BoolWeight() {}
     };
+    struct CoolWeight : public Weight {
+	CoolWeight(int) {}
+    };
 }
 void f() {
     auto helper = [&](const Xapian::Weight& weight,
@@ -11,5 +14,6 @@ void f() {
     };
 #define TEST_WEIGHTING_SCHEME(W, ...) helper(W(__VA_ARGS__), #W, "" #__VA_ARGS__)
     TEST_WEIGHTING_SCHEME(Xapian::BoolWeight);
+    TEST_WEIGHTING_SCHEME(Xapian::CoolWeight, 42);
 #undef TEST_WEIGHTING_SCHEME
 }
