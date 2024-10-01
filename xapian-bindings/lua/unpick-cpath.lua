c0cd7bb77a7d07ee0c51dea0ab0ef79fb7de58cd
Olly Betts <olly@survex.com>
2024-10-01 15:26:35 +1300

    fix reversed condition

--- xapian-bindings/lua/unpick-cpath.lua
+++ xapian-bindings/lua/unpick-cpath.lua
@@ -4,7 +4,7 @@ so = string.lower(arg[1])
 for e in string.gmatch(string.gsub(package.cpath,"\\","/"), "[^;]*") do
   io.stderr:write("cpath entry: "..e.."\n")
   l,s = string.match(e, "^(/.*)/%?(%.%a+)$")
-  if l ~= nil then
+  if l == nil then
     l,s = string.match(e, "^(%a:/.*)/%?(%.%a+)$")
   end
   if l ~= nil and string.lower(s) ~= ".lua" then
