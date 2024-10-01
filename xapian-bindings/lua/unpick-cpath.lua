f8e4277d880899c7b5320f162d5adb69aebb76a7
Olly Betts <olly@survex.com>
2024-10-01 14:51:35 +1300

    Fiddlin'

--- xapian-bindings/lua/unpick-cpath.lua
+++ xapian-bindings/lua/unpick-cpath.lua
@@ -4,10 +4,10 @@ so = string.lower(arg[1])
 for e in string.gmatch(string.gsub(package.cpath,"\\","/"), "[^;]*") do
   io.stderr:write("cpath entry: "..e.."\n")
   l,s = string.match(e, "^(/.*)/%?(%.%a+)$")
-  if not l then
-    l,s = string.match(e, "^(%a:?/.*)/%?(%.%a+)$")
+  if l ~= nil then
+    l,s = string.match(e, "^(%a:/.*)/%?(%.%a+)$")
   end
-  if l and string.lower(s) ~= ".lua" then
+  if l ~= nil and string.lower(s) ~= ".lua" then
     if so == "" or string.lower(s) == so then
       print(l..";"..s)
       break
