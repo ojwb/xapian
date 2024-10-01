0583e201ace785a6dbeeb06e4417407a1d8c80d9
Olly Betts <olly@survex.com>
2024-10-01 13:41:44 +1300

    Debug lua more

--- xapian-bindings/lua/unpick-cpath.lua
+++ xapian-bindings/lua/unpick-cpath.lua
@@ -1,6 +1,8 @@
 require("package")
+require("io")
 so = string.lower(arg[1])
 for e in string.gmatch(string.gsub(package.cpath,"\\","/"), "[^;]*") do
+  io.stderr:write("cpath entry: "..e.."\n")
   l,s = string.match(e, "^(/.*)/%?(%.%a+)$")
   if l and string.lower(s) ~= ".lua" then
     if so == "" or string.lower(s) == so then
