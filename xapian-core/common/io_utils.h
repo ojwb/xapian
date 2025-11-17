78e59d76694f9bbe740531e2670e085228eafa4b
Olly Betts <olly@survex.com>
2025-11-18 07:06:17 +1300

    Use overlapped IO for pread()/pwrite() equivs

--- xapian-core/common/io_utils.h
+++ xapian-core/common/io_utils.h
@@ -225,7 +225,8 @@ static inline void io_protect_from_write(int fd) {
       defined __APPLE__ || \
       defined __NetBSD__ || \
       defined __OpenBSD__ || \
-      defined __sun__
+      defined __sun__ || \
+      defined __WIN32__
     // The maximum off_t value worked in testing on:
     // * Cygwin 3.6.5
     // * DragonFlyBSD 6.4.2
@@ -234,6 +235,7 @@ static inline void io_protect_from_write(int fd) {
     // * NetBSD 10.0
     // * OpenBSD 7.5
     // * Solaris 10 and 11.4
+    // * Microsoft Windows Server 2025 10.0.26100
     (void)lseek(fd, std::numeric_limits<off_t>::max(), SEEK_SET);
 #elif defined __EMSCRIPTEN__
     if constexpr (sizeof(off_t) > 4) {
