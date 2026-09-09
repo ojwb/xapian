d23fb6cfec00b8ccce7b3d86238df1405f89b8c1
Olly Betts <olly@survex.com>
2026-09-09 13:50:39 +1200

    Try turning off for platforms where it fails

--- xapian-core/tests/api_backend.cc
+++ xapian-core/tests/api_backend.cc
@@ -2082,19 +2082,18 @@ DEFINE_TESTCASE(checksingletable1, glass || honey) {
 
     TEST_EQUAL(Xapian::Database::check(db_path + "/postlist"), 0);
 
-#if 0 //def __DragonFly__
+#ifdef __DragonFly__
     // FIXME: Fails to link with:
     // api_backend.cc:2091: error: undefined reference to 'std::filesystem::current_path[abi:cxx11]()'
     // /usr/include/c++/8.0/bits/fs_path.h:184: error: undefined reference to 'std::filesystem::__cxx11::path::_M_split_cmpts()'
     // api_backend.cc:2092: error: undefined reference to 'std::filesystem::current_path(std::filesystem::__cxx11::path const&)'
     // api_backend.cc:2096: error: undefined reference to 'std::filesystem::current_path(std::filesystem::__cxx11::path const&)'
+#elif defined _MSC_VER
+    // FIXME: When compiled with MSVC, apitest.exe seems to exit when it gets
+    // to here.
 #else
     auto cwd = std::filesystem::current_path();
-    //std::filesystem::current_path(std::filesystem::path(db_path));
-    if (chdir(db_path.c_str()) < 0) {
-        tout << "errno: " << strerror(errno) << '\n';
-        FAIL_TEST("chdir() failed");
-    }
+    std::filesystem::current_path(std::filesystem::path(db_path));
     try {
         TEST_EQUAL(Xapian::Database::check("postlist"), 0);
     } catch (...) {
